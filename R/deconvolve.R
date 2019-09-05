248218fb0454dd76ae151b46b1848ee15121d760
Justin Revenaugh <justinr@umn.edu>
2019-09-05 17:27:59 -0500

    Touched everything to fix age problem with git

--- R/deconvolve.R
+++ R/deconvolve.R
@@ -5,25 +5,25 @@
 #' @param s real-valued univariate or multivariate (as matrix columns) timeseries.
 #' @param f real-valued filter to deconvolve.
 #' @param type one of water-level ("wl", default), damped "least squqres" ("dls") or no regularization ("noreg").
-#' See detail for more information  
+#' See detail for more information
 #' @param metric one of "max" (default), "median" or "percentile."  Together with \code{alpha} this determines
 #' the regularization of the deconvolution.
-#' @param alpha multiplier of the max or median value of the denominator for the corresponding \mode{metric},
+#' @param alpha multiplier of the max or median value of the denominator for the corresponding \code{metric},
 #' If \code{metric} is "percentile", this specifies the percentile value of the denominator to use in regularization.
 #' @details If the convolved filter has spectral zeros
 #' (or near zeros), any noise in that band will be greatly exaggerated by deconvolution. To control this,
 #' it is common to regularize the denominator during spectral division (deconvolution in the frequency domain).
 #' Two methods are provided. (1) water-level ("wl") doesn't allow the value of the denominator to drop below the scaled metric.
 #' Visually, the denominator plotted against frequency is filled with water up to the scaled metric, thus
-#' filling in "holes."  (2) damped "least-squares" ("dls") simply adds the scaled metric to the 
+#' filling in "holes."  (2) damped "least-squares" ("dls") simply adds the scaled metric to the
 #' denominator at all frequencies.  Visually, this shifts the baseline of the denominator up.  While this
 #' reduces the noise-amplification of zeros and near-zeros, it biases results at all frequencies.
 #' Type "noreg" applies no regularization and will produce useless results for noisy inputs.
-#' 
+#'
 #' The scaled metric is one of: alpha * max( D ), alpha * median( D ) or quantile( D, alpha ) where D
 #' is the denominator (function of frequency).
-#' 
-#' Deconvolution is circular.  Long filters will cycle around.  
+#'
+#' Deconvolution is circular.  Long filters will cycle around.
 #' Padding \code{s} prior to deconvolution is often desirable.
 #' @return Deconvolved timeseries
 #' @export
@@ -49,7 +49,7 @@
 #' plotl( r, main = "Damped LS" )
 #' lines( rd, col = "red" )
 
-deconvolve <- function( s, f, type = c( "wl", "dls", "noreg" ), 
+deconvolve <- function( s, f, type = c( "wl", "dls", "noreg" ),
                        alpha = 0.1, metric = c( "median", "max", "percentile" ) ) {
   # match inputs
   type <- match.arg( type )
@@ -72,7 +72,7 @@ deconvolve <- function( s, f, type = c( "wl", "dls", "noreg" ),
   n <- max( n, length( f ) )
   npad <- nextn( n, 2 )
   l <- npad / 2 + 1
-  
+
   # prepare inverse filter
   f <- c( f, rep( 0, npad - n ) )
   F <- fft( f )
@@ -81,11 +81,11 @@ deconvolve <- function( s, f, type = c( "wl", "dls", "noreg" ),
                  "max" = alpha * max( D ),
                  "median" = alpha * median( D ),
                  "percentile" = quantile( D, alpha ) )
-  
+
   if ( type == "wl" ) {
     ind <- which( D < reg )
     D[ind] <- reg
-  } 
+  }
   if ( type == "dls" ) D <- D + reg
 
   # loop over input signals
@@ -96,6 +96,6 @@ deconvolve <- function( s, f, type = c( "wl", "dls", "noreg" ),
     st <- fft( S, inverse = TRUE ) / n
     s[,i] <- st[1:n]
   }
-  
+
   return( Re( s ) )
 }
